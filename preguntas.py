1a6bfdcbacc097c02d7b764e8469b9fd709b92fb
NordeyPR <102186979+NordeyPR@users.noreply.github.com>
2022-04-12 11:46:17 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -11,6 +11,8 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 
 
 """
+from collections import Counter
+from copy import copy
 import csv
 dic = dict()
 dic_b = dic.copy()
@@ -24,6 +26,7 @@ dic_b["E"]= 0
 with open("data.csv", "r",) as file:
    Lab_1 = csv.reader(file,delimiter = "\t")
    lista = list(Lab_1)
+   listaa = list(Lab_1)
    #lista.sort()
 
 def pregunta_01():
@@ -277,31 +280,16 @@ def pregunta_07():
     ]
 
     """
-    pr_7_0 = [linea[0] for linea in lista if linea[1] == "0"]
-    pr_7_1 = [linea[0] for linea in lista if linea[1] == "1"]
-    pr_7_2 = [linea[0] for linea in lista if linea[1] == "2"]
-    pr_7_3 = [linea[0] for linea in lista if linea[1] == "3"]
-    pr_7_4 = [linea[0] for linea in lista if linea[1] == "4"]
-    pr_7_5 = [linea[0] for linea in lista if linea[1] == "5"]
-    pr_7_6 = [linea[0] for linea in lista if linea[1] == "6"]
-    pr_7_7 = [linea[0] for linea in lista if linea[1] == "7"]
-    pr_7_8 = [linea[0] for linea in lista if linea[1] == "8"]
-    pr_7_9 = [linea[0] for linea in lista if linea[1] == "9"]
-    lin_0 = 0 , pr_7_0
-    lin_1 = 1 , pr_7_1
-    lin_2 = 2 , pr_7_2
-    lin_3 = 3 , pr_7_3
-    lin_4 = 4 , pr_7_4
-    lin_5 = 5 , pr_7_5
-    lin_6 = 6 , pr_7_6
-    lin_7 = 7 , pr_7_7
-    lin_8 = 8 , pr_7_8
-    lin_9 = 9 , pr_7_9
-
-    pr_7 = dict()
-    pr_7 = [lin_0 , lin_1 , lin_2 ,lin_3 , lin_4 , lin_5 , lin_6 , lin_7 , lin_8 , lin_9]
+    data = [linea[:2] for linea in lista]
+    resul = {}
+    for letra , valor in data:
+        valor = int(valor)
+        if valor in resul.keys():
+             resul[valor].append(letra)
+        else:
+            resul[valor] = [letra]
     
-    return pr_7
+    return sorted(resul.items())
 
 
 def pregunta_08():
@@ -326,7 +314,39 @@ def pregunta_08():
     ]
 
     """
-    return
+    with open("data.csv", "r",) as file:
+        Lab_1 = csv.reader(file,delimiter = "\t")
+        listaa = list(Lab_1)
+    
+
+    pr_7_0 = sorted([linea[0] for linea in listaa if linea[1] == "0"])
+    pr_7_1 = sorted([linea[0] for linea in listaa if linea[1] == "1"])
+    pr_7_2 = sorted([linea[0] for linea in listaa if linea[1] == "2"])
+    pr_7_3 = sorted([linea[0] for linea in listaa if linea[1] == "3"])
+    pr_7_4 = sorted([linea[0] for linea in listaa if linea[1] == "4"])
+    pr_7_5 = sorted([linea[0] for linea in listaa if linea[1] == "5"])
+    pr_7_6 = sorted([linea[0] for linea in listaa if linea[1] == "6"])
+    pr_7_7 = sorted([linea[0] for linea in listaa if linea[1] == "7"])
+    pr_7_8 = sorted([linea[0] for linea in listaa if linea[1] == "8"])
+    pr_7_9 = sorted([linea[0] for linea in listaa if linea[1] == "9"])
+
+    lin_0 = 0 , sorted(list(set(pr_7_0)))
+    lin_1 = 1 , sorted(list(set(pr_7_1)))
+    lin_2 = 2 , sorted(list(set(pr_7_2)))
+    lin_3 = 3 , sorted(list(set(pr_7_3)))
+    lin_4 = 4 , sorted(list(set(pr_7_4)))
+    lin_5 = 5 , sorted(list(set(pr_7_5)))
+    lin_6 = 6 , sorted(list(set(pr_7_6)))
+    lin_7 = 7 , sorted(list(set(pr_7_7)))
+    lin_8 = 8 , sorted(list(set(pr_7_8)))
+    lin_9 = 9 , sorted(list(set(pr_7_9)))
+
+   
+    pr_7 = sorted([lin_0 , lin_1 , lin_2 ,lin_3 , lin_4 , lin_5 , lin_6 , lin_7 , lin_8 , lin_9])
+         
+
+        
+    return pr_7
 
 
 def pregunta_09():
@@ -349,7 +369,23 @@ def pregunta_09():
     }
 
     """
-    return
+    
+    data=open('data.csv', 'r').readlines()
+    data = [row[0:-1] for row in data]
+    data = csv.reader(data,delimiter = "\t")
+    data = [row[4].split(",") for row in data]
+
+    result_9 = []
+
+    for letra in data:
+        data = [row.split(':') for row in letra]
+        result_9 += data
+        claves = []
+        for a in result_9:
+                claves.append(a[0])
+        clavesval = Counter(claves).most_common(10)
+        resultado = sorted(clavesval, reverse= False)
+    return dict(resultado)
 
 
 def pregunta_10():
@@ -370,7 +406,15 @@ def pregunta_10():
 
 
     """
-    return
+    result_10 = []
+    for letra in lista:
+        letra_10 = letra[0]
+        valor4 = [letra[4].split(",")]
+        valor_4 = len(valor4[0])
+        valor3 = [letra[3].split(",")]
+        valor_3 = len(valor3[0])
+        result_10.append((letra_10 , valor_3 , valor_4))
+    return result_10
 
 
 def pregunta_11():
@@ -391,7 +435,32 @@ def pregunta_11():
 
 
     """
-    return
+    data=open('data.csv', 'r').readlines()
+    data = [row[0:-1] for row in data]
+    data = csv.reader(data,delimiter = "\t")
+    data = [row[3].split(",") for row in data]
+    result_9 = []
+
+    for letra in data:
+        result_9 += data
+        clave = []
+        for e in result_9:
+            clave.append(e[0])
+            resultado = sorted(list(set(clave)))
+
+    columna1y3=[(z[3].split(','),z[1]) for z in lista]
+
+    diccio = {}
+    for clave in resultado:
+        suma = 0
+        for e in columna1y3:
+            for letra in e[0]:
+                if letra == clave:
+                    suma = suma + int(e[1])
+            dicc = {clave : suma}
+            diccio.update(dicc)
+
+    return diccio
 
 
 def pregunta_12():
@@ -409,4 +478,37 @@ def pregunta_12():
     }
 
     """
-    return
+    import csv
+    data=open('data.csv', 'r').readlines()
+    ult = data.copy()
+    data = [row[0:-1] for row in data]
+    data = csv.reader(data,delimiter = "\t")
+    data = [(row[4].split(",") ) for row in data]
+
+
+    listasumas = []
+    col1 = [row[0] for row in ult]
+
+    for letra in data:
+        data = [row.split(":") for row in letra]
+        total = []
+        for a in data:
+            val = a[1:]
+            total += val
+        valores = [int(row) for row in total]
+        val = sum(valores)
+        listasumas.append(val)
+    grupos = sorted(list(zip(col1,listasumas)))
+    lisletras = []
+    lisfinal = []
+    for a in ult:
+        lisletras.append(a[0])
+    for letra in set(lisletras):
+        counter = 0
+        for lista in grupos:
+            if lista[0] == letra:
+                counter= counter + lista[1]
+        lisfinal.append((letra,counter))
+
+
+    return dict(lisfinal)
